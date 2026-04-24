0860f3970e75cce267a628223a4a5b8e2e3ca204
Florian Hartung <60144801+florianhartung@users.noreply.github.com>
2026-04-24 21:48:00 +0200

    fix: impermanence

--- nixosModules/impermanence/default.nix
+++ nixosModules/impermanence/default.nix
@@ -4,7 +4,7 @@
 # - command as shortcut for fs-diff.sh
 # - groups for commonly persisted files/directories. e.g. "bluetooth", "network", "ssh", "varlog"
 
-{ lib, config, inputs, ... }:
+{ lib, config, inputs, pkgs, ... }:
 let cfg = config.impermanence;
 in {
 
@@ -45,43 +45,75 @@ in {
       enable = true;
       supportedFilesystems = [ "btrfs" ];
 
-      postResumeCommands = lib.mkAfter ''
-        mkdir -p /mnt
-        # We first mount the btrfs root to /mnt
-        # so we can manipulate btrfs subvolumes.
-        mount -o subvol=/ ${cfg.devPath} /mnt
+      systemd = {
+        enable = true;
+        # service options taken from https://github.com/ElvishJerricco/impermanence
+        services.wipe-root = {
+          # Specify dependencies explicitly
+          unitConfig.DefaultDependencies = false;
+          # The script needs to run to completion before this service is done
+          serviceConfig.Type = "oneshot";
+          # This service is required for boot to succeed
+          requiredBy = [ "initrd.target" ];
+          # Should complete before any file systems are mounted
+          before = [ "sysroot.mount" ];
 
-        # While we're tempted to just delete /root and create
-        # a new snapshot from /root-blank, /root is already
-        # populated at this point with a number of subvolumes,
-        # which makes `btrfs subvolume delete` fail.
-        # So, we remove them first.
-        #
-        # /root contains subvolumes:
-        # - /root/var/lib/portables
-        # - /root/var/lib/machines
-        #
-        # I suspect these are related to systemd-nspawn, but
-        # since I don't use it I'm not 100% sure.
-        # Anyhow, deleting these subvolumes hasn't resulted
-        # in any issues so far, except for fairly
-        # benign-looking errors from systemd-tmpfiles.
-        btrfs subvolume list -o /mnt/root |
-        cut -f9 -d' ' |
-        while read subvolume; do
-          echo "deleting /$subvolume subvolume..."
-          btrfs subvolume delete "/mnt/$subvolume"
-        done &&
-        echo "deleting /root subvolume..." &&
-        btrfs subvolume delete /mnt/root
+          # Wait for the disk to appear
+          requires = [ "initrd-root-device.target" ];
+          after = [
+            "initrd-root-device.target"
+            # Allow hibernation to resume before trying to alter any data
+            "local-fs-pre.target"
+          ];
 
-        echo "restoring blank /root subvolume..."
-        btrfs subvolume snapshot /mnt/root-blank /mnt/root
+          script = ''
+            mkdir -p /mnt
+            # We first mount the btrfs root to /mnt
+            # so we can manipulate btrfs subvolumes.
+            mount -o subvol=/ ${cfg.devPath} /mnt
 
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt and continue on the boot process.
-        umount /mnt
-      '';
+            # While we're tempted to just delete /root and create
+            # a new snapshot from /root-blank, /root is already
+            # populated at this point with a number of subvolumes,
+            # which makes `btrfs subvolume delete` fail.
+            # So, we remove them first.
+            #
+            # /root contains subvolumes:
+            # - /root/var/lib/portables
+            # - /root/var/lib/machines
+            #
+            # I suspect these are related to systemd-nspawn, but
+            # since I don't use it I'm not 100% sure.
+            # Anyhow, deleting these subvolumes hasn't resulted
+            # in any issues so far, except for fairly
+            # benign-looking errors from systemd-tmpfiles.
+            btrfs subvolume list -o /mnt/root |
+            cut -f9 -d' ' |
+            while read subvolume; do
+              echo "deleting /$subvolume subvolume..."
+              btrfs subvolume delete "/mnt/$subvolume"
+            done &&
+            echo "deleting /root subvolume..." &&
+            btrfs subvolume delete /mnt/root
+
+            echo "restoring blank /root subvolume..."
+            btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+            # Once we're done rolling back to a blank snapshot,
+            # we can unmount /mnt and continue on the boot process.
+            umount /mnt
+          '';
+        };
+        extraBin = {
+          "mkdir" = "${pkgs.coreutils}/bin/mkdir";
+          "date" = "${pkgs.coreutils}/bin/date";
+          "stat" = "${pkgs.coreutils}/bin/stat";
+          "mv" = "${pkgs.coreutils}/bin/mv";
+          "find" = "${pkgs.findutils}/bin/find";
+          "btrfs" = "${pkgs.btrfs-progs}/bin/btrfs";
+          # mount & umount already exist
+        }; # NOTE: path = [...]; doesnt work for initrd, use full paths in your script or extraBin
+      };
     };
 
   };
